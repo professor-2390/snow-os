79ca8d6e9562b87b1cfb7344651a4d39dabd1a99
root <sadmansakibkhan4@gmail.com>
2021-02-07 01:38:43 -0500

    mouse cursor and keyboard

---
+++ README.md
@@ -0,0 +1,3 @@
+# snow-os
+
+It is an open source os developed by me and it will update a lot in this branch there is a mouse and keyboard
---
+++ kernel/src/kernel.cpp
@@ -0,0 +1,17 @@
+#include "kernelUtil.h"
+
+extern "C" void _start(BootInfo* bootInfo){
+
+    KernelInfo kernelInfo = InitializeKernel(bootInfo);
+    PageTableManager* pageTableManager = kernelInfo.pageTableManager;
+
+    GlobalRenderer->Print("Kernel Initialized Successfully");
+
+    
+
+    while(true){
+        ProcessMousePacket();
+    }
+
+    while(true);
+}
